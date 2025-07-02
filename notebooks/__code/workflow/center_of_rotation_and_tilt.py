0 and 360 center of rotation fixed. Improved log folder destination (works on hype). Fixed bug in sorting of angles. this refs #23

notebooks/__code/workflow/center_of_rotation_and_tilt.py
@@ -259,7 +259,7 @@ class CenterOfRotationAndTilt(Parent):
 
         image_0_degree = self.image_0_degree
         image_180_degree = self.image_180_degree
-        image_360_degree = self.image_360_degree
+        # image_360_degree = self.image_360_degree
 
         # logging.info(f"{image_0_degree.dtype = }")
         # logging.info(f"{np.shape(image_0_degree) = }")
@@ -272,12 +272,13 @@ class CenterOfRotationAndTilt(Parent):
         self.slide_value = int(height/2)
 
         display(widgets.HTML("Select the slice to use to calculate the center of rotation"))
-        max_value = np.max([image_0_degree, image_180_degree, image_360_degree])
+        # max_value = np.max([image_0_degree, image_180_degree, image_360_degree])
+        max_value = np.max([image_0_degree, image_180_degree])
         # max_value = 4 # DEBUG
 
         def plot_images(slice_value=int(height/2), vmin=0, vmax=max_value):
 
-            fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(15,5))
+            fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10,5))
             
             axs[0].imshow(image_0_degree, cmap='viridis', vmin=vmin, vmax=vmax)
             axs[0].set_title("0 / 0")
@@ -287,9 +288,9 @@ class CenterOfRotationAndTilt(Parent):
             axs[1].set_title(f"{self.parent.final_list_of_angles[self.index_180_degree]} / 180")
             axs[1].axhline(slice_value, color='blue', linestyle='--')
 
-            axs[2].imshow(image_360_degree, cmap='viridis', vmin=vmin, vmax=vmax)
-            axs[2].set_title(f"{self.parent.final_list_of_angles[self.index_360_degree]} / 360")
-            axs[2].axhline(slice_value, color='blue', linestyle='--')
+            # axs[2].imshow(image_360_degree, cmap='viridis', vmin=vmin, vmax=vmax)
+            # axs[2].set_title(f"{self.parent.final_list_of_angles[self.index_360_degree]} / 360")
+            # axs[2].axhline(slice_value, color='blue', linestyle='--')
 
             plt.tight_layout()
 
@@ -317,10 +318,10 @@ class CenterOfRotationAndTilt(Parent):
         display(widgets.HTML("Horizontal line shows the slide used to calculate the center of rotation"))
         display(widgets.HTML("<hr>"))
 
-        display(widgets.HTML("Do you want to use all the data from 0 to 180, or 0 to 360 degrees to calculate the center of rotation?"))
-        self.cor_selection = widgets.RadioButtons(options=["180 degree", "360 degree"],
-                                         descriptions="Select mode:")
-        display(self.cor_selection)
+        # display(widgets.HTML("Do you want to use all the data from 0 to 180, or 0 to 360 degrees to calculate the center of rotation?"))
+        # self.cor_selection = widgets.RadioButtons(options=["180 degree", "360 degree"],
+        #                                  descriptions="Select mode:")
+        # display(self.cor_selection)
 
     def calculate_center_of_rotation(self):
         self.calc_cor_with_algotom()
@@ -333,7 +334,6 @@ class CenterOfRotationAndTilt(Parent):
             return
         
         logging.info(f"calculate center of rotation using algotom (auto mode)")
-        logging.info(f"\tmode selected: {self.cor_selection.value}")
        
         logging.info(f"\tworking with sinogram in log mode!")
         sinogram_of_normalized_images_log = np.moveaxis(self.parent.normalized_images_log, 1, 0) # [slice, angle, width]
@@ -341,21 +341,21 @@ class CenterOfRotationAndTilt(Parent):
 
         slice_value = self.plot_slice_to_use.result
 
-        if self.cor_selection.value == "180 degree":
-            center_of_rotation = find_center_vo(sinogram_of_normalized_images_log[self.slide_value][0:slice_value],
-                                                ncore=NUM_THREADS)
-        else:
-            try:
-                center_of_rotation = find_center_360(sinogram_of_normalized_images_log[self.slide_value][:],
-                                                    win_width=800,
-                                                    ncore=NUM_THREADS)
-                if type(center_of_rotation) == list:
-                    center_of_rotation = center_of_rotation[0]
+        # if self.cor_selection.value == "180 degree":
+        center_of_rotation = find_center_vo(sinogram_of_normalized_images_log[self.slide_value][0:slice_value],
+                                            ncore=NUM_THREADS)
+        # else:
+        #     try:
+        #         center_of_rotation = find_center_360(sinogram_of_normalized_images_log[self.slide_value][:],
+        #                                             win_width=800,
+        #                                             ncore=NUM_THREADS)
+        #         if type(center_of_rotation) == list:
+        #             center_of_rotation = center_of_rotation[0]
 
-            except ValueError as e:
-                logging.error(f"Error: {e}")
-                logging.info(f"Error: {e}")
-                center_of_rotation = None
+        #     except ValueError as e:
+        #         logging.error(f"Error: {e}")
+        #         logging.info(f"Error: {e}")
+        #         center_of_rotation = None
        
         logging.info(f"center of rotation = {center_of_rotation}")
         self.parent.configuration.center_of_rotation = center_of_rotation

notebooks/__code/workflow/load.py
@@ -28,7 +28,7 @@ class Load(Parent):
         if data_type in [DataType.reconstructed, DataType.extra]:
             working_dir = self.parent.working_dir[DataType.processed]
         else:
-            working_dir = self.parent.working_dir[DataType.top]
+            working_dir = self.parent.working_dir[data_type]
 
         if DEBUG:
             self.data_selected(debug_folder[self.parent.MODE][data_type])
@@ -52,10 +52,10 @@ class Load(Parent):
         if data_type in [DataType.reconstructed, DataType.extra]:
             working_dir = self.parent.working_dir[DataType.processed]
         else:
-            working_dir = self.parent.working_dir[DataType.top]
+            working_dir = self.parent.working_dir[data_type]
 
         if DEBUG:
-            working_dir = debug_folder[self.parent.MODE][data_type]
+            working_dir = debug_folder[self.parent.MODE][DataType.top]
             if not os.path.exists(working_dir):
                 return
             #list_images = glob.glob(os.path.join(working_dir, "*_0045_*.tif*"))
@@ -223,6 +223,17 @@ class Load(Parent):
     def save_list_of_angles(self, list_of_images):
         if self.parent.retrieve_angle_value_from_metadata:
             self.retrieve_angle_value_from_metadata_file(list_of_images)
+            
+            self.parent.final_list_of_angles = [float(angle) for angle in self.parent.final_list_of_angles]
+            # we need to sort the angles and then sort the list of images the same way
+            sorted_indices = sorted(range(len(self.parent.final_list_of_angles)), key=lambda i: self.parent.final_list_of_angles[i])
+            self.parent.final_list_of_angles = [self.parent.final_list_of_angles[i] for i in sorted_indices]
+            self.parent.final_list_of_angles_rad = np.deg2rad(self.parent.final_list_of_angles)
+            self.parent.list_of_images[DataType.sample] = [list_of_images[i] for i in sorted_indices]
+
+            logging.info(f"Angle values retrieved from metadata file and sorted: {self.parent.final_list_of_angles}")
+            logging.info(f"list of files sorted the same way: {self.parent.list_of_images[DataType.sample]}")
+
         else:
             self.retrieve_angle_value_from_file_name(list_of_images)
 