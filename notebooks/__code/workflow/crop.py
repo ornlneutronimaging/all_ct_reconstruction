manual input of ascii list of angles. Improving visualization. Fixing normalization using same trick as jupyter notebooks normalization_tof. this refs #57

notebooks/__code/workflow/crop.py
@@ -82,11 +82,12 @@ class Crop(Parent):
             _data = self.parent.master_3d_data_array[DataType.sample]
         else:
            _data = self.parent.normalized_images
-        integrated: NDArray[np.generic] = np.min(_data, axis=0)
+        integrated_min: NDArray[np.generic] = np.min(_data, axis=0)
+        integrated_mean: NDArray[np.generic] = np.mean(_data, axis=0)
 
         height: int
         width: int
-        height, width = integrated.shape
+        height, width = integrated_min.shape
 
         # Set default crop boundaries based on debug mode
         default_left: int
@@ -112,7 +113,7 @@ class Crop(Parent):
         if default_bottom < 0:
             default_bottom = height - abs(default_bottom)
 
-        max_value: float = np.max(integrated)
+        max_value: float = np.max(integrated_mean)
 
         vmax_default_value: float
         if before_normalization:
@@ -120,7 +121,7 @@ class Crop(Parent):
         else:
             vmax_default_value = 1
 
-        def plot_crop(left_right: list, top_bottom: list, vmin_vmax: list) -> Tuple[int, int, int, int]:
+        def plot_crop(left_right: list, top_bottom: list, vmin_vmax: list, data_type: str) -> Tuple[int, int, int, int]:
             """
             Inner function to plot the crop region visualization.
             
@@ -145,6 +146,11 @@ class Crop(Parent):
             vmin: float = vmin_vmax[0]
             vmax: float = vmin_vmax[1]
 
+            if data_type == "Min":
+                integrated: NDArray[np.generic] = integrated_min
+            else:
+                integrated: NDArray[np.generic] = integrated_mean
+
             fig, axs = plt.subplots(figsize=(7,7)) 
             img = axs.imshow(integrated, vmin=vmin, vmax=vmax)
             plt.colorbar(img, ax=axs, shrink=0.5)
@@ -167,24 +173,29 @@ class Crop(Parent):
                                        left_right=widgets.SelectionRangeSlider(options=list(range(width)),
                                                                                 index=(default_left, default_right),
                                                                                 description='Left/Right:',
-                                                                                layout=widgets.Layout(width="50%"),
+                                                                                layout=widgets.Layout(width="80%"),
                                                                                 continuous_update=False,
                                                                                 ),
 
                                         top_bottom=widgets.SelectionRangeSlider(options=list(range(height)),
                                                                                 index=(default_top, default_bottom),
                                                                                 description='Top/Bottom:',
-                                                                                layout=widgets.Layout(width="50%"),
+                                                                                layout=widgets.Layout(width="80%"),
                                                                                 continuous_update=False,
                                                                                 ),
                                         vmin_vmax=widgets.FloatRangeSlider(description='Vmin/Vmax:',
                                                                            options=list(np.linspace(0, max_value, num=100)),
-                                                                           layout=widgets.Layout(width="50%"),
+                                                                           layout=widgets.Layout(width="80%"),
                                                                            min=0,
                                                                            max=max_value,
                                                                            continuous_update=False,
                                                                            value=(0, vmax_default_value)
                                         ),
+                                        data_type=widgets.RadioButtons(options=["Min", "Mean"],
+                                                                       description='Data type:',
+                                                                       disabled=False,
+                                                                       value="Min",
+                                                                       layout=widgets.Layout(width="50%"))
         )
         display(self.display_roi)
 

notebooks/__code/workflow/load.py
@@ -132,7 +132,7 @@ class Load(Parent):
         if DEBUG:
             self.data_selected(debug_folder[default_detector_type][self.parent.MODE][data_type])
             logging.info(f"{default_detector_type = }")
-            self.parent.working_dir[DataType.nexus] = debug_folder[default_detector_type][self.parent.MODE][DataType.nexus]
+            self.parent.working_dir[DataType.nexus] = debug_folder[default_detector_type][self.parent.MODE].get(DataType.nexus, None)
             logging.info(f"DEBUG MODE: {data_type} folder selected: {debug_folder[default_detector_type][self.parent.MODE][data_type]}")
             _list_sep = self.parent.working_dir[DataType.sample].split(os.sep)
             # facility = _list_sep[0]
@@ -220,42 +220,68 @@ class Load(Parent):
         display(HTML(f"<span style='color:green'>{len(list_images)} images have been selected as {self.data_type}!</span>"))
         self.parent.list_of_images[self.data_type] = list_images
 
-    def select_percentage_of_data_to_use(self):
+    def use_all_or_fraction(self):
        
-        def display_data_to_use(slider_index):
+        self.select_widget = widgets.RadioButtons(options=['Use all data for reconstruction', 
+                                                      'Use a fraction of the data for reconstruction'],
+                                              description='',
+                                              disabled=False,
+                                              value='Use all data for reconstruction',
+                                              style={'description_width': 'initial'}
+                                              )
         
-            self.determine_projections_angles_to_use(slider_index)
-            display(HTML(f"{self.parent.temp_nbr_of_images_will_be_used} images will be used for the reconstruction"))
+        display(self.select_widget)
 
+    def select_percentage_of_data_to_use(self):
+
+        if self.select_widget.value == 'Use all data for reconstruction':
+            self.parent.percentage_of_data_to_use_for_reconstruction = 100
+            self.determine_projections_angles_to_use(percentage_to_use=100)
+            display(HTML(f"All images will be used for the reconstruction: {self.parent.temp_nbr_of_images_will_be_used} images"))
+            return
+        
+        def display_data_to_use(slider_index, preview=False):
+        
+            logging.info(f"Percentage of data to use for reconstruction: {slider_index}%")
+            display(HTML(f"Processing how many images will be used for the reconstruction ..."))
+            self.determine_projections_angles_to_use(percentage_to_use=slider_index)
+            display(HTML(f"{self.parent.temp_nbr_of_images_will_be_used} images will be used for the reconstruction"))
+    
             logging.info("Visualizing angles selected for the projections.")
             full_list_of_angles_rad = self.parent.full_list_of_angles_rad
             logging.info(f"\tFull list of angles (radians): {full_list_of_angles_rad}")
             logging.info(f"\tSelected angles (radians): {self.parent.temp_final_list_of_angles_rad}")
-            fig, ax = plt.subplots(figsize=(5, 5), subplot_kw={'projection': 'polar'})
-            ax.plot(full_list_of_angles_rad, np.ones(len(full_list_of_angles_rad))*2, marker='o', linestyle='None', color='blue', markersize=5, label='All Angles')
-            ax.set_rmax(2)
-            ax.set_rticks([0.5, 1, 1.5])
-            selected_list_of_angles_rad = self.parent.temp_final_list_of_angles_rad
-            ax.plot(selected_list_of_angles_rad, np.ones(len(selected_list_of_angles_rad))*2, marker='o', linestyle='None', color='green', markersize=10, label='Selected Angles')
-            ax.legend()
+            
+            if preview:
+                fig, ax = plt.subplots(figsize=(5, 5), subplot_kw={'projection': 'polar'})
+                ax.plot(full_list_of_angles_rad, np.ones(len(full_list_of_angles_rad))*2, marker='o', linestyle='None', color='blue', markersize=5, label='All Angles')
+                ax.set_rmax(2)
+                ax.set_rticks([0.5, 1, 1.5])
+                selected_list_of_angles_rad = self.parent.temp_final_list_of_angles_rad
+                ax.plot(selected_list_of_angles_rad, np.ones(len(selected_list_of_angles_rad))*2, marker='o', linestyle='None', color='green', markersize=10, label='Selected Angles')
+                ax.legend()
+            
             return slider_index
         
         self.percentage_of_data_to_use_widget = interactive(display_data_to_use,
-                                                              slider_index=widgets.IntSlider(value=PERCENTAGE_OF_DATA_TO_USE_FOR_RECONSTRUCTION,
-                                                                                           min=5,
-                                                                                           max=100,
-                                                                                           step=1,
-                                                                                           layout=widgets.Layout(width='100%')))
+                                                            slider_index=widgets.IntSlider(value=PERCENTAGE_OF_DATA_TO_USE_FOR_RECONSTRUCTION,
+                                                                                            min=5,
+                                                                                            max=100,
+                                                                                            step=1,
+                                                                                            continuous_update=False,
+                                                                                            layout=widgets.Layout(width='100%')),
+                                                               preview=widgets.Checkbox(value=False,),
+                                                               )
 
         display(self.percentage_of_data_to_use_widget)
 
-    def on_percentage_to_use_change(self, change):
-        new_value = change['new']
-        list_tiff = self.parent.list_of_images[DataType.sample]
-        nbr_images = int(new_value / 100 * len(list_tiff))
-        self.number_of_images_to_use.value = f"{nbr_images} images will be used for the reconstruction"
-        self.determine_projections_angles_to_use()
-        self.visualize_angles_selected()
+    # def on_percentage_to_use_change(self, change):
+    #     new_value = change['new']
+    #     list_tiff = self.parent.list_of_images[DataType.sample]
+    #     nbr_images = int(new_value / 100 * len(list_tiff))
+    #     self.number_of_images_to_use.value = f"{nbr_images} images will be used for the reconstruction"
+    #     self.determine_projections_angles_to_use()
+    #     self.visualize_angles_selected()
 
     def how_to_retrieve_angle_value(self):
         self.how_to_retrieve_angle_value_widget = widgets.RadioButtons(
@@ -318,6 +344,9 @@ class Load(Parent):
             return
 
         display(widgets.HTML(f"<font color='green'><b>Number of angles from ASCII file and number of images match!</b></font>"))
+
+        # make sure the angles values are within 0-360 degrees
+        list_of_angles = np.array(list_of_angles) % 360
 
         self.parent.final_list_of_angles = list_of_angles.tolist()
         self.parent.final_list_of_angles_rad = np.deg2rad(list_of_angles).tolist()
@@ -511,29 +540,44 @@ class Load(Parent):
     def determine_projections_angles_to_use(self, percentage_to_use=50):
         """Retrieve list of TIFF files from the selected folder."""
         logging.info(f"Determine list of projections to use according to percentage selected ...")
+        
+        # if retrieve_list_of_files_and_angles:
+        logging.info(f"\tRetrieving list of files and angles ...")
         list_of_tiff = self.parent.list_of_images[DataType.sample]
         list_of_tiff.sort()
         _dict = self.retrieve_list_of_files_and_angles()
         list_of_tiff = _dict['list_of_images']
         list_of_angles_deg = _dict['list_of_angles_deg']
         # list_of_angles_rad = _dict['list_of_angles_rad']
-
+        logging.info(f"Done retrieving list of files and angles.")
+        
         nbr_images_to_use = int(percentage_to_use / 100 * len(list_of_tiff))
         if nbr_images_to_use < 5:
             nbr_images_to_use = 5
         logging.info(f"\tNumber of projections to use: {nbr_images_to_use}")
         list_of_angles_deg = [float(_value) for _value in self.parent.final_list_of_angles]
         self.parent.full_list_of_angles_rad = self.parent.final_list_of_angles_rad
+
+        if percentage_to_use == 100:
+            logging.info(f"\tUsing all angles for the reconstruction.")
+            self.parent.temp_final_list_of_angles = list_of_angles_deg
+            self.parent.temp_final_list_of_angles_rad = self.parent.final_list_of_angles_rad
+            self.parent.temp_list_of_images = list_of_tiff
+            self.parent.temp_nbr_of_images_will_be_used = len(list_of_tiff)
+            return
+
+        logging.info(f"\tSelecting angles using farthest point sampling ...")
         selected_angles = farthest_point_sampling(list_of_angles_deg, nbr_images_to_use)
+        logging.info(f"\tDone selecting angles.")
 
         logging.info(f"Making sure 0 and 180 degrees are included in the selected angles.")
         if not list_of_angles_deg[0] in selected_angles:
             logging.info(f"\tAdding 0 degrees to the selected angles.")
             selected_angles.insert(0, np.float64(list_of_angles_deg[0]))
 
-        if not list_of_angles_deg[-1] in selected_angles:
-            logging.info(f"\tAdding 180 degrees to the selected angles.")
-            selected_angles.append(np.float64(list_of_angles_deg[-1]))
+        # if not list_of_angles_deg[-1] in selected_angles:
+        #     logging.info(f"\tAdding 180 degrees to the selected angles.")
+        #     selected_angles.append(np.float64(list_of_angles_deg[-1]))
 
         logging.info(f"\tSelected angles: {selected_angles}")
         selected_indices = [np.where(list_of_angles_deg == angle)[0][0] for angle in selected_angles]

notebooks/__code/workflow/normalization.py
@@ -123,7 +123,7 @@ class Normalization(Parent):
         self.use_sample_roi_ui = widgets.Checkbox(value=False,
                                         description='Use sample ROI normalization',
                                         layout=widgets.Layout(width="100%"))
-        vertical_layout = widgets.VBox([self.use_proton_charge_ui,
+        vertical_layout = widgets.VBox([# self.use_proton_charge_ui,
                                         # self.use_frames_ui,
                                         self.use_roi_ui,
                                         self.use_sample_roi_ui])
@@ -143,7 +143,7 @@ class Normalization(Parent):
 
         # integrated_images = np.log(np.min(self.parent.master_3d_data_array[DataType.sample], axis=0))
         sample_images = self.parent.master_3d_data_array[DataType.sample]
-        integrated_images = np.min(sample_images, axis=0)
+        integrated_images = np.mean(sample_images, axis=0)
         height, width = np.shape(integrated_images)
 
         def plot_roi(left_right, top_bottom):
@@ -178,17 +178,26 @@ class Normalization(Parent):
             default_left = default_top = 0
             default_right = default_bottom = 20
 
+        if default_left >= width:
+            default_left = 0
+        if default_right >= width:
+            default_right = width // 10
+        if default_top >= height:
+            default_top = 0
+        if default_bottom >= height:
+            default_bottom = height // 10
+
         self.display_roi = interactive(plot_roi,
                                        left_right=widgets.SelectionRangeSlider(options=list(range(width)),
                                                                                 index=(default_left, default_right),
                                                                                 description='Left-Right:',
                                                                                 orientation='horizontal',
-                                                                                layout=widgets.Layout(width='50%')),
+                                                                                layout=widgets.Layout(width='80%')),
                                         top_bottom=widgets.SelectionRangeSlider(options=list(range(height)),
                                                                                 index=(default_top, default_bottom),
                                                                                 description='Top-Bottom:',
                                                                                 orientation='horizontal',
-                                                                                layout=widgets.Layout(width='50%')),
+                                                                                layout=widgets.Layout(width='80%')),
                                       )
                                         
         display(self.display_roi)
@@ -248,12 +257,17 @@ class Normalization(Parent):
                 coeff *= ob_roi_counts / sample_roi_counts
 
             if not (dc_data_combined is None):
-                num = sample_data - dc_data_combined
-                den = ob_data_combined - dc_data_combined
-                normalized_sample = np.true_divide(num, den, dtype=np.float32) * coeff
-            else:
-                normalized_sample = np.true_divide(sample_data, ob_data_combined, dtype=np.float32) * coeff
 
+                normalized_sample = np.divide(np.subtract(sample_data, dc_data_combined, dtype=np.float32),
+                                             np.subtract(ob_data_combined, dc_data_combined, dtype=np.float32),
+                                             out=np.zeros_like(sample_data, dtype=np.float32),
+                                             where=(ob_data_combined - dc_data_combined) != 0,
+                                            ) * coeff
+            else:
+                normalized_sample = np.divide(sample_data, ob_data_combined,
+                                              out=np.zeros_like(sample_data, dtype=np.float32),
+                                              where=ob_data_combined != 0) * coeff
+                
             if use_sample_roi:
                 sample_roi_counts = np.median(normalized_sample[top: bottom+1, left: right+1])
                 coeff = 1 / sample_roi_counts
@@ -261,19 +275,20 @@ class Normalization(Parent):
 
             # remove NaN and Inf
             # logging.info(f"removing NaN and Inf values (nan->0, -inf->0, inf->1)")
-            normalized_sample = tomopy.misc.corr.remove_nan(normalized_sample, val=0, ncore=NUM_THREADS)
-            normalized_sample = np.nan_to_num(normalized_sample, nan=0, posinf=1, neginf=0)
+            # normalized_sample = tomopy.misc.corr.remove_nan(normalized_sample, val=0, ncore=NUM_THREADS)
+            # normalized_sample = np.nan_to_num(normalized_sample, nan=0, posinf=1, neginf=0)
 
             # logging_3d_array_infos(message="after normalization", array=normalized_sample)
             # normalized_data.append(normalized_sample) 
 
             normalized_sample[normalized_sample > 1] = 1
+            normalized_sample[normalized_sample < 0] = 0
             normalized_data[_index] = normalized_sample[:]
 
         # remove negative values
         logging.info(f"removing nan and negative values (set to 0)")
-        normalized_data = tomopy.misc.corr.remove_nan(normalized_data, val=0, ncore=NUM_THREADS)
-        normalized_data = tomopy.misc.corr.remove_neg(normalized_data, val=0, ncore=NUM_THREADS)
+        # normalized_data = tomopy.misc.corr.remove_nan(normalized_data, val=0, ncore=NUM_THREADS)
+        # normalized_data = tomopy.misc.corr.remove_neg(normalized_data, val=0, ncore=NUM_THREADS)
 
         self.parent.normalized_images = np.squeeze(np.asarray(normalized_data, dtype=np.float32))
         logging_3d_array_infos(message="normalized images", array=self.parent.normalized_images)